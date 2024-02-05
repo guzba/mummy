8b3ee3ceb9cd491d739082c612e6fc65799f8bcb
Ryan Oldenburg <ryan@guzba.com>
2024-02-05 02:39:07 -0600

    add examples

---
+++ examples/basic_router_path_params.nim
@@ -0,0 +1,15 @@
+import mummy, mummy/routers
+
+## http://localhost:8080/objects/abc
+
+proc objectsHandler(request: RoutedRequest) =
+  var headers: HttpHeaders
+  headers["Content-Type"] = "text/plain"
+  request.respond(200, headers, "Object: " & request.pathParams["id"])
+
+var router: Router
+router.get("/objects/@id", objectsHandler)
+
+let server = newServer(router)
+echo "Serving on http://localhost:8080"
+server.serve(Port(8080))
